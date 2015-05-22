86df63cff68fe0e3f1502ee96f9e5e122932d835
Peter Horvath <peter.horvath@lhsystems.com>
2015-05-22 16:23:03 +0200

    Refactored from gradle to maven

--- src/main/java/coreservlets/BidBean2.java
+++ src/main/java/coreservlets/BidBean2.java
@@ -1,65 +1,69 @@
 package coreservlets;
 
-import javax.faces.context.*;
-import javax.faces.component.*;
-import javax.faces.validator.*;
-import javax.faces.application.*;
-import javax.faces.bean.*;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 
 @ManagedBean
 public class BidBean2 {
-  private String userID;
-  private String keyword;
-  private Double bidAmount;
-  private Integer bidDuration;
+    private String userID;
+    private String keyword;
+    private Double bidAmount;
+    private Integer bidDuration;
 
-  public String getUserID() { return(userID); }
-
-  public void setUserID(String userID) {
-    this.userID = userID;
-  }
-
-  public String getKeyword() { return(keyword); }
-
-  public void setKeyword(String keyword) {
-    this.keyword = keyword;
-  }
-
-  public Double getBidAmount() { return(bidAmount); }
-
-  public void setBidAmount(Double bidAmount) {
-    this.bidAmount = bidAmount;
-  }
-
-  public Integer getBidDuration() { return(bidDuration); }
-
-  public void setBidDuration(Integer bidDuration) {
-    this.bidDuration = bidDuration;
-  }
-
-  public String doBid() {
-    doBusinessLogicForValidData();
-    return("show-bid2");
-  }
-  
-  private void doBusinessLogicForValidData() {}
-
-  public void validateBidAmount(FacesContext context,
-                                UIComponent componentToValidate,
-                                Object value)
-      throws ValidatorException {
-    double bidAmount = (Double)value;
-    double previousHighestBid = currentHighestBid();
-    if (bidAmount <= previousHighestBid) {
-      FacesMessage message =
-        new FacesMessage("Bid must be higher than current " +
-                         "highest bid ($" +
-                         previousHighestBid + ").");
-      throw new ValidatorException(message);
+    public String getUserID() {
+        return (userID);
     }
-  }
 
-  public double currentHighestBid() {
-    return(0.23); // Get from database in real life
-  }
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getKeyword() {
+        return (keyword);
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+
+    public Double getBidAmount() {
+        return (bidAmount);
+    }
+
+    public void setBidAmount(Double bidAmount) {
+        this.bidAmount = bidAmount;
+    }
+
+    public Integer getBidDuration() {
+        return (bidDuration);
+    }
+
+    public void setBidDuration(Integer bidDuration) {
+        this.bidDuration = bidDuration;
+    }
+
+    public String doBid() {
+        doBusinessLogicForValidData();
+        return ("show-bid2");
+    }
+
+    private void doBusinessLogicForValidData() {
+    }
+
+    public void validateBidAmount(FacesContext context, UIComponent componentToValidate, Object value) throws ValidatorException {
+        double bidAmount = (Double) value;
+        double previousHighestBid = currentHighestBid();
+
+        if (bidAmount <= previousHighestBid) {
+            FacesMessage message = new FacesMessage("Bid must be higher than current " + "highest bid ($" + previousHighestBid + ").");
+            throw new ValidatorException(message);
+        }
+    }
+
+    public double currentHighestBid() {
+        return (0.23); // Get from database in real life
+    }
 }
--- src/main/java/org/hp/HP.java
+++ src/main/java/org/hp/HP.java
@@ -1,13 +1,17 @@
 package org.hp;
 
+import java.io.InputStream;
 import java.io.Serializable;
-import java.util.Date;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseId;
 
 import org.joda.time.DateTime;
 import org.joda.time.format.ISODateTimeFormat;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
 
 /**
  * Created by U292148 on 2014.09.26..
@@ -33,4 +37,22 @@ public class HP implements Serializable {
     public void setHp1(HP1 hp1) {
         this.hp1 = hp1;
     }
+
+    public StreamedContent getImage(){
+        FacesContext context = FacesContext.getCurrentInstance();
+
+        if (context.getCurrentPhaseId() == PhaseId.RENDER_RESPONSE) {
+            // So, we're rendering the HTML. Return a stub StreamedContent so that it will generate right URL.
+            return new DefaultStreamedContent();
+        }
+        else {
+            // So, browser is requesting the image. Return a real StreamedContent with the image bytes.
+            String studentId = context.getExternalContext().getRequestParameterMap().get("studentId");
+            InputStream imageStream = this.getClass().getResourceAsStream("cat.jpg");
+            return new DefaultStreamedContent(imageStream);
+
+//            return new DefaultStreamedContent(new ByteArrayInputStream(student.getImage()));
+        }
+    }
+
 }
